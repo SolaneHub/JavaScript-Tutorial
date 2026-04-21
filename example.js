bbe050bb85a8cad453e62eee9321c550b0712ff4
SolaneHub <simonelostimolo@gmail.com>
2026-04-21 16:34:00 +0200

    docs: :memo: added warming up exercise for variable types

--- example.js
+++ example.js
@@ -15,3 +15,22 @@ console.log(typeof 10n); // BigInt
 console.log(typeof Symbol("x")); // Symbol
 console.log(typeof []); // Object
 console.log(typeof {}); // Object
+
+/**
+ * * Exercise 2
+ * ? TASK: Which of these variables are reassignable and which are not?
+ * ! NOTE: Both `var` and `let` can be reassigned. `const` (constant) cannot.
+ * TODO: Demonstrate reassignment for each type.
+ */
+
+var a = 1; 
+let b = 2; 
+const c = 3;
+
+a = 10; // OK: var is reassignable
+b = 20; // OK: let is reassignable
+// c = 30; // Uncaught TypeError: Assignment to constant variable.
+
+console.log("a (var):", a);
+console.log("b (let):", b);
+console.log("c (const):", c);
